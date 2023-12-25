2b7dc0661b90df4e0ce786c4ce658f0187c998a5
Gautam-2604 <gaurisariag@gmail.com>
2023-12-25 12:06:00 +0530

    App.jsx created

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,56 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { ToDoProvider } from './context'
+import { useEffect } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setToDos] = useState([])
 
+  const addToDo = (todo)=>{
+    setToDos((prev)=>[{id : Date.now(), ...todo},...prev])
+  }
+
+  const updateToDo = (id, todo)=>{
+    setToDos((prev)=>prev.map((prevToDo)=> (prevToDo.id===id ? todo : prevToDo)))
+  }
+
+  const deleteToDo = (id)=>{
+    setToDos((prev)=>prev.filter((todo) =>todo.id !== id))
+  }
+
+  const toggleComplete = (id)=>{
+    setToDos((prev)=>prev.map((prevToDo)=>prevToDo.id === id ? {...prevToDo, completed : !prevToDo.completed}:prevToDo ))
+  }
+
+  //Local Storage access function
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todos"))
+
+    if (todos && todos.length > 0) {
+      setToDos(todos)
+    }
+  },[])
+
+  useEffect(()=>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+  },[todos])
+    
+  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <ToDoProvider value={{todos, addToDo, updateToDo, deleteToDo, toggleComplete}}>
+      <div className="bg-[#172842] min-h-screen py-8">
+                <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                    <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                    <div className="mb-4">
+                        {/* Todo form goes here */} 
+                    </div>
+                    <div className="flex flex-wrap gap-y-3">
+                        {/*Loop and Add TodoItem here */}
+                    </div>
+                </div>
+            </div>
+    </ToDoProvider>
   )
 }
 
